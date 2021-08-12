38959e2deb7eb1df3a9d76abae1d865dc03a893e
Tom Pavelec <tom.pavelec@gmail.com>
2021-08-12 11:01:57 +0200

    fix: phpstan for php 8

--- src/GenericException/IJsonRPCAwareException.php
+++ src/GenericException/IJsonRPCAwareException.php
@@ -7,13 +7,6 @@ namespace Contributte\JsonRPC\GenericException;
 interface IJsonRPCAwareException extends \Throwable
 {
 
-	// @codingStandardsIgnoreStart
-	/**
-	 * @return string
-	 */
-	public function getMessage();
-	// @codingStandardsIgnoreEnds
-
 	public function getErrorCode(): int;
 
 	public function getGeneralMessage(): string;
